2b5ed8dfaad9666b4c3064afa8ed2418cdc0d05b
ThomazDev <thomazrichardydossantos2010@gmail.com>
2026-03-03 09:26:04 -0300

    Improve user creation and validation logic
    Refactor user creation logic to validate input and increment idCounter correctly.

--- App.java
+++ App.java
@@ -1,11 +1,13 @@
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class App {
+
     static List<User> users = new ArrayList<>();
     static Scanner input = new Scanner(System.in);
-    static int idCounter = 1;
+    static int idCounter = 0;
 
     public static void main(String[] args) {
         int decision = 0;
@@ -20,8 +22,8 @@ public class App {
             if (decision == 1) {
                 try {
                     int option = 0;
-                    System.out.println("Type 1 to create a user \nType 2 to view information " +
-                            "\nType 3 to update information \nType 4 to delete user");
+                    System.out.println("Type 1 to create a user \nType 2 to view information "
+                            + "\nType 3 to update information \nType 4 to delete user");
                     option = input.nextInt();
 
                     switch (option) {
@@ -48,19 +50,28 @@ public class App {
 
     public static void createUser() {
         try {
+            //String email = "gmail.com ".formatted()
             User userObject = new User();
             input.nextLine();
             System.out.println("What name you want to add?");
             userObject.setName(input.nextLine());
+            if (userObject.getName().equals("")) {
+                System.out.println("VALUE INVALID");
+                return;
+            }
             System.out.println("What your age?");
             userObject.setAge(input.nextInt());
             input.nextLine();
+            if (userObject.getAge() < 0) {
+                System.out.println("VALUE INVALID");
+                return;
+            }
             System.out.println("What email you want to add?");
             userObject.setEmail(input.nextLine());
             System.out.println("What phone number you want to add?");
             userObject.setPhoneNumber(input.nextLine());
-            userObject.setId(idCounter);
             idCounter++;
+            userObject.setId(idCounter);
             users.add(userObject);
         } catch (Exception e) {
             System.out.println("Value Invalid");
@@ -72,7 +83,7 @@ public class App {
         for (User userObject : users) {
             System.out.println("Id: " + userObject.getId() + "\nName: " + userObject.getName() + "\nAge: "
                     + userObject.getAge() + "\nEmail: "
-                    + userObject.getEmail() + "\nPhone: " + userObject.getPhoneNumber());
+                    + userObject.getEmail() + "\nPhone: " + userObject.getPhoneNumber() + "\n-----------------------------------------");
         }
     }
 
@@ -82,12 +93,15 @@ public class App {
             System.out.println("Type the id you want to acess:");
             int id = input.nextInt();
             input.nextLine();
+            boolean found = false;
+
             for (User userObject : users) {
                 if (userObject.getId() == id) {
                     System.out.println("Name: " + userObject.getName() + "\nAge: " + userObject.getAge() + "\nEmail: "
                             + userObject.getEmail() + "\nPhone: " + userObject.getPhoneNumber());
                     System.out.println("What information you want update?");
                     update = input.nextLine().trim().toLowerCase();
+
                     switch (update) {
                         case "name":
                             System.out.println("What name you want to add?");
@@ -96,6 +110,7 @@ public class App {
                         case "age":
                             System.out.println("What your age?");
                             userObject.setAge(input.nextInt());
+                            input.nextLine();
                             break;
                         case "email":
                             System.out.println("What email you want to add?");
@@ -106,13 +121,16 @@ public class App {
                             userObject.setPhoneNumber(input.nextLine());
                             break;
                     }
-
+                    found = true;
+                    break;
                 }
-                return;
             }
-            System.out.println("No found User");
+
+            if (!found) {
+                System.out.println("User not found");
+            }
         } catch (Exception e) {
-            System.out.println("Value Invalid");
+            System.out.println("Invalid value");
             input.nextLine();
         }
     }
@@ -127,7 +145,6 @@ public class App {
                     users.remove(i);
                     System.out.println("User delete successfuly");
                     return;
-
                 }
             }
         } catch (Exception e) {
